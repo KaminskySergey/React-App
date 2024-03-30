5f9c81e75aa7d2c4d622f5a6ed5b9d85c6cec3eb
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2024-03-30 19:00:46 +0200

    fi

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider  store={store}> 
     <PersistGate loading={null}
         persistor={persistor}>
-    <BrowserRouter basename='/todo'>
+    <BrowserRouter basename='/'>
     <App />
     </BrowserRouter>
     </PersistGate>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/todo",
+  base: "./",
   plugins: [react()],
   server: {
     port: 3000,
